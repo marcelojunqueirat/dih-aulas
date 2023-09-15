7cbc1d21494c11c11513d0ba08c3eeaff9fd11a1
Marcelo Junqueira <marcelojxt@gmail.com>
2023-09-14 21:53:57 -0300

    s02m02-aula3 ternario

--- m02/s01/Aula03OperadoresAritmeticos/src/aula3/Ternario.java
+++ m02/s01/Aula03OperadoresAritmeticos/src/aula3/Ternario.java
@@ -3,9 +3,12 @@ package aula3;
 public class Ternario {
     public static void main(String[] args) {
         int idade = 18;
-
-        String maioridade = (idade >=18) ? "adulto" : "menor de idade";
-
+        String maioridade = (idade >= 18) ? "adulto" : "menor de idade";
         System.out.println(maioridade);
+
+        int num = 3;
+        boolean ehPar = num % 2 == 0;// ? true : false;
+        String parOuimpar = num % 2 == 0 ? "par" : "impar" ;
+        System.out.printf("O numero %d é %s.", num, parOuimpar );
     }
 }
