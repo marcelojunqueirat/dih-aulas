0a4b9ddf5ea1d4784373d7490cbccb5294cceafb
Marcelo Junqueira Teixeira <marcelo.teixeira@softplan.com.br>
2024-01-30 20:58:26 -0300

    aula02 m03s02 - 1

--- m03/s02/aula02/src/App.jsx
+++ m03/s02/aula02/src/App.jsx
@@ -5,10 +5,19 @@ function App() {
   const [joke, setJoke] = useState(null)
   const [newJoke, setNewJoke] = useState(0)
 
+  const [user, setUser] = useState({})
+
   useEffect(() => {
     fetch('https://api.chucknorris.io/jokes/random')
       .then(response => response.json())
       .then(data => setJoke(data.value))
+      .catch(error => console.error(error))
+  }, [newJoke])
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/marcelojunqueirat')
+      .then(response => response.json())
+      .then(data => setUser(data))
       .catch(error => console.error(error))
   }, [newJoke])
 
@@ -17,6 +26,18 @@ function App() {
       <h3>Welcome to Chuck Norris Jokes</h3>
       <h2>{joke && joke}</h2>
       <button onClick={() => setNewJoke(newJoke + 1)}>New Joke?</button>
+
+      <hr />
+
+      <div>
+        <img id='avatar' src={user.avatar_url} alt="" />
+      </div>
+
+      <h3>{user.name}</h3>
+      <h4>{user.bio}</h4>
+      <h4>Seguindo: {user.following}</h4>
+      <h4>Seguidores: {user.followers}</h4>
+      <h4>{user.location}</h4>
     </>
   )
 }
