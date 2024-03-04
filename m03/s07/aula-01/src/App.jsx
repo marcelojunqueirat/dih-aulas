eb674c33e440a6b709a86babba46cc04b7126d58
Marcelo Junqueira Teixeira <marcelojxt@gmail.com>
2024-03-04 20:09:36 -0300

    aula 01 - lembrando react

---
+++ m03/s07/aula-01/src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import Card from './components/Card'
+import { dataCrypto } from './database/data'
+
+function App() {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    setList(dataCrypto);
+  }, dataCrypto);
+
+  return (
+    <>
+      <Card list={list} />
+    </>
+  )
+}
+
+export default App
---
+++ m03/s07/aula-01/src/components/Card/index.jsx
@@ -0,0 +1,25 @@
+import './style.css'
+function Card({list}) {
+  console.log(list)
+  return (
+    <ul>
+      {list.map((item, i) => (
+        <li key={i}>
+          <div id="perfil-data">
+            <img id="perfil-photo" src={item.foto_perfil} alt={item.criador} />
+            {item.nome}
+            <span>{item.nickcriador}</span>
+          </div>
+          <img src={item.imagem} alt={item.categoria} />
+
+          <div>
+            <div>R$ {item.precoreal}</div>
+            <b>{item.preco} {item.moeda}</b>
+          </div>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default Card;
