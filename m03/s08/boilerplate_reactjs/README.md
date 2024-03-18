2342eb6f0df53935471a22b6644624e5c4f35323
Marcelo Junqueira Teixeira <marcelo.teixeira@softplan.com.br>
2024-03-18 16:56:30 -0300

    m3s08

---
+++ m03/s08/boilerplate_reactjs/README.md
@@ -0,0 +1,3 @@
+# React + Vite
+
+## Testes com JEST
---
+++ m03/s08/boilerplate_reactjs/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+function HomePage() {
+    return (
+        <div className="content">
+        <h1>Home Page - {import.meta.env.VITE_APP_NAME}</h1>
+        </div>
+    )
+}
+
+export default HomePage
